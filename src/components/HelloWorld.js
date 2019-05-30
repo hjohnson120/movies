c4e2736bfced14cf53d3a5004cd91f90575ed469
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-30 16:36:28 -0400

    added first line

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 
 class HelloWorld extends Component {
   render() {
-    return <h1>Hello, World!</h1>
+    return (
+      <>
+        <h1>Hello, World!</h1>
+        <h1>Hi Heather!!</h1>
+      </>
+    )
   }
 }
 
