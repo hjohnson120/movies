66d6d572909e5fdcf68a2096a632e144e081434b
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-06-01 12:15:15 -0400

    Random component

---
+++ src/Pages/Movie.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Movie extends Component {
+  render() {
+    return <div />
+  }
+}
+
+export default Movie
---
+++ src/components/RandomMovie.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class RandomMovie extends Component {
+  render() {
+    return <div />
+  }
+}
+
+export default RandomMovie
